634dd94499a1b7213af7821029cb0d67dd2abd55
Dolf <adolfo.mttgz@gmail.com>
2023-10-20 09:59:35 -0300

    in main branch

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ if __name__ == '__main__':
     print(rsp1.text)
     # print(rsp.status_code)
     # print(rsp.text)
+
+# nothing to commit, working tree clean
+#
+# C:\Users\Hewlett Packard\PycharmProjects\api_stand_tests (master -> origin)
+# λ git push origin master
+# Enter passphrase for key '/c/Users/Hewlett Packard/.ssh/id_rsa':
+# Everything up-to-date
+#
+# C:\Users\Hewlett Packard\PycharmProjects\api_stand_tests (master -> origin)
+# λ git push -u origin master
+# Enter passphrase for key '/c/Users/Hewlett Packard/.ssh/id_rsa':
+# Everything up-to-date
+# branch 'master' set up to track 'origin/master'.
+#
+# C:\Users\Hewlett Packard\PycharmProjects\api_stand_tests (master -> origin)
