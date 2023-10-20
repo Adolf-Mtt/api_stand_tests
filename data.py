075246fe3fec2f9c0b3ddf458893c3f8307a87c6
Dolf <adolfo.mttgz@gmail.com>
2023-10-20 10:05:15 -0300

    solo data.py

--- data.py
+++ data.py
@@ -13,18 +13,50 @@ product_ids = {
 
 variable = { "function" : 1, "object": 2 }
 
+#
+# 9 Cursos
+# Curso de Estrategias para Aprender en Línea Efectivamente 2019
+# Curso de Estrategias para Aprender en Línea Efectivamente 2019
+#
+# 0%
+#
+# Curso de DevOps con GitLab
+# Curso de DevOps con GitLab
+#
+# 0%
+#
+# Curso Profesional de Git y GitHub
+# Curso Profesional de Git y GitHub
+#
+# 7%
+#
+# Curso de Educación Financiera y Finanzas Personales
+# Curso de Educación Financiera y Finanzas Personales
+#
+# 0%
+#
+# Curso de Gestión Efectiva del Tiempo
+# Curso de Gestión Efectiva del Tiempo
+#
+# 0%
+#
+# Curso de  Prework: Configuración de Entorno de Desarrollo en Linux
+# Curso de Prework: Configuración de Entorno de Desarrollo en Linux
+#
+# 0%
+#
+# Curso de Fundamentos de Pruebas de Software
+# Curso de Fundamentos de Pruebas de Software
+#
+# 18%
+#
+# Curso de Introducción a la Automatización de Pruebas
+# Curso de Introducción a la Automatización de Pruebas
+#
+# 0%
+#
+# Curso de Automatización de Pruebas UI con Cypress
+# Curso de Automatización de Pruebas UI con Cypress
+#
+# 0%
 
-# nothing to commit, working tree clean
-#
-# C:\Users\Hewlett Packard\PycharmProjects\api_stand_tests (master -> origin)
-# λ git push origin master
-# Enter passphrase for key '/c/Users/Hewlett Packard/.ssh/id_rsa':
-# Everything up-to-date
-#
-# C:\Users\Hewlett Packard\PycharmProjects\api_stand_tests (master -> origin)
-# λ git push -u origin master
-# Enter passphrase for key '/c/Users/Hewlett Packard/.ssh/id_rsa':
-# Everything up-to-date
-# branch 'master' set up to track 'origin/master'.
-#
-# C:\Users\Hewlett Packard\PycharmProjects\api_stand_tests (master -> origin)
